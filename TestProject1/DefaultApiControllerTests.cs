4f6730d720bad38689d98a0e0791951ea6a93896
Roman Stepanov <ronan1810@gmail.com>
2024-04-18 01:45:16 +0300

    Unit Tests

---
+++ TestProject1/DefaultApiControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using Core.Interfaces;
+using Core.Models;
+using IO.Swagger.Controllers;
+using IO.Swagger.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using Core.Interfaces;
+using Core.Models;
+using IO.Swagger.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestProject1
+{
+    public class DefaultApiControllerTests
+    {
+        [Fact]
+        public void CalculatePost_ValidParameters_ReturnsOkResultWithResult()
+        {
+            // Arrange
+            var mockCalcService = new Mock<ICalcService>();
+            var body = new CalculateBody { Num1 = 5, Num2 = 3 };
+            var operation = "add";
+            var resultResponse = new ResultResponce { Result = 8 };
+
+            mockCalcService.Setup(service => service.GetResult(body.Num1, body.Num2, operation))
+                .Returns(resultResponse);
+
+            var controller = new DefaultApiController(mockCalcService);
+
+            // Act
+            var result = controller.CalculatePost(body, operation);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<InlineResponse2001>(okResult.Value);
+            Assert.Equal(resultResponse.Result, returnValue.Result);
+        }
+    }
+}
---
+++ TestProject1/LoginApiControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using Microsoft.Extensions.Configuration;
+using Core.Interfaces;
+using Core.Models;
+using IO.Swagger.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestProject1
+{
+
+    public class LoginApiControllerTests
+    {
+        [Fact]
+        public void Login_ValidUser_ReturnsOkResultWithToken()
+        {
+            // Arrange
+            var mockUserService = new Mock<IUserService>();
+            var mockConfiguration = new Mock<IConfiguration>();
+            var user = new UserModel { Username = "Test", Password = "Test" };
+            var token = "testToken";
+
+            mockUserService.Setup(service => service.AuthenticateUser(It.IsAny<UserModel>())).Returns(user);
+            mockUserService.Setup(service => service.GenerateJSONWebToken(user, It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(token);
+            mockConfiguration.Setup(config => config["Jwt:Key"]).Returns("testKey");
+            mockConfiguration.Setup(config => config["Jwt:Issuer"]).Returns("testIssuer");
+
+            var controller = new LoginApiController(mockUserService.Object, mockConfiguration.Object);
+
+            // Act
+            var result = controller.Login(user);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<Dictionary<string, string>>(okResult.Value);
+            Assert.False(string.IsNullOrEmpty(returnValue["token"]));
+        }
+    }
+}
